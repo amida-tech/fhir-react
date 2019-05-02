f0016930606531a7a350992bb051fd8e2011ac26
jmfadeley@gmail.com <jmfadeley@gmail.com>
2019-05-02 12:51:26 -0400

    Changes to keep up with the Stevensons.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Patient from '../components/Patient';
+import PatientView from '../components/PatientView';
+import { patient as Marion, info } from '../data/Marion';
+
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+
 
 import { Button, Welcome } from '@storybook/react/demo';
 
@@ -12,4 +16,11 @@ storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo(
 storiesOf('Patient', module)
   .add('Hello World', () => (
     <Patient/>
+  ));
+
+storiesOf('PatientView', module)
+  .add('WHADDUP', () => (
+    <PatientView
+      patient={Marion}
+      info={info}/>
   ));
