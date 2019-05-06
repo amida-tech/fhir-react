93897860babd95cc443452b77304c131a6a3a88d
jmfadeley@gmail.com <jmfadeley@gmail.com>
2019-05-06 15:50:18 -0400

    Let's have some fun.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -23,7 +23,7 @@ storiesOf('Patient', module)
     
   ));
 
-storiesOf('PatientView', module)
+storiesOf('TALES OF SUSPENSE AND HUMAN NAMES', module)
   .add('Because Riley made me change it', () => (
     <HumanName humanName={Marion.name} 
       nameInfo={info.nameInfo}/>
