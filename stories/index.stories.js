a066f82262b145ab9d7b682edb8f7502b7600a0c
Grant Orndorff <grant@amida.com>
2019-05-16 11:42:22 -0400

    importing all examples for a dropdown in storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,26 +2,42 @@ import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { storiesOf } from '@storybook/react';
 import { linkTo } from '@storybook/addon-links';
-import { object, withKnobs } from '@storybook/addon-knobs';
+import { select, object, withKnobs } from '@storybook/addon-knobs';
 import { Welcome } from '@storybook/react/demo';
 import { patient as Marion, info } from '../data/Marion';
 import Patient from '../components/Patient';
 import storybookTheme from '../themes/xd';
 import DefaultTheme from '../themes/default';
-
+import { patientExamples } from '../data/examples';
 
 storiesOf('Patient', module)
   .addDecorator(withKnobs)
   .addParameters({ options: { theme: storybookTheme }, viewport: { defaultViewport: 'iphone6' } })
   .add('Default Patient Theme', () => {
     const theme = createMuiTheme(DefaultTheme);
-
     return (
       <div>
         <MuiThemeProvider theme={theme}>
           <Patient
             patient={object('Patient', Marion)}
             info={object('Info', info)}
+          />
+        </MuiThemeProvider>
+      </div>
+    );
+  })
+  .add('With Patient Dropdown', () => {
+    const theme = createMuiTheme(DefaultTheme);
+    const patientOptions = {
+      Marion,
+      ...patientExamples,
+    };
+    return (
+      <div>
+        <MuiThemeProvider theme={theme}>
+          <Patient
+            patient={select('Patient', patientOptions, Marion, 'Patient')}
+            info={info}
           />
         </MuiThemeProvider>
       </div>
